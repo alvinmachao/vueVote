824c1522309c2e5bb1d9939a9ec7350efe14f789
alvinmachao <821603741@qq.com>
2017-09-21 14:21:33 +0800

    update

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,6 +1,8 @@
 var fs = require('fs')
 
 module.exports = {
+  userFile: './data/users.json',
+  actionFile: './data/actions.json',
   /**
    * 返回数据格式
    * status 0 =>成功 1 =>失败 msg:信息
@@ -9,6 +11,53 @@ module.exports = {
   data: {
     status: 0,
     msg: '成功'
+  },
+  /**
+   * gennerator生成器
+   * @param promiseFn promise
+   */
+  genFactory: function (fn, args) {
+    return function * () {
+      yield fn(args)
+    }
+  },
+  /**
+   * 异步自执行run函数
+   * @param gen
+   */
+  run: function (gen) {
+    var g
+    if (typeof gen.next === 'function') {
+      g = gen
+    } else {
+      g = gen()
+    }
+
+    function next (data) {
+      var tmp = g.next(data)
+      if (tmp.done) {
+        return
+      } else {
+        tmp.value.then(next)
+      }
+    }
+
+    next()
+  },
+  readFile: function (fileName) {
+    return new Promise(function (resolve, reject) {
+      fs.readFile(fileName, function (err, data) {
+        if (err) {
+          reject(err)
+        } else {
+          if (!data) {
+            data = []
+          }
+          data = JSON.parse(data)
+          resolve(data)
+        }
+      })
+    })
   },
   getAllUser () {
     var users = fs.readFileSync('./data/users.json', 'utf-8')
@@ -23,19 +72,23 @@ module.exports = {
       return []
     }
     return JSON.parse(actions)
-  },
+  }
+  ,
   writeUsers (users) {
     fs.writeFileSync('./data/users.json', JSON.stringify(users))
-  },
+  }
+  ,
   writeActions (actions) {
     fs.writeFileSync('./data/actions.json', JSON.stringify(actions))
-  },
+  }
+  ,
   register (user, callback) {
     var users = this.getAllUser()
     users.length > 0 ? (user.id = Number(users[users.length - 1].id) + 1) : user.id = 1
     users.push(user)
     this.writeUsers(users, callback)
-  },
+  }
+  ,
   isExist (content, data) {
     var users = this.getAllUser()
     var reg = /^\d+$/
